2c9e0bfa28971d4f78d28401c43aeb4707cc205d
Mamoru Yanagisawa <72236660+mamoru-x-yanagisawa@users.noreply.github.com>
2022-08-25 05:24:44 +0900

    add autofill test
    Signed-off-by: Mamoru Yanagisawa <72236660+mamoru-x-yanagisawa@users.noreply.github.com>

--- src/webdriver/autofill.ts
+++ src/webdriver/autofill.ts
@@ -1,9 +1,29 @@
+/**
+ * Copyright 2022 NTT Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Special operation type that is not added by DOM event.
+ */
+
 import WebBrowserWindow from "@/capturer/browser/window/WebBrowserWindow";
 import { By, WebElement } from "selenium-webdriver";
 import WebDriverClient from "./WebDriverClient";
 import HTMLParser from "node-html-parser";
 
-type InputValueSet = {
+export type InputValueSet = {
   locatorType: "id" | "xpath";
   locator: string;
   locatorMatchType: "equals" | "regex";
@@ -34,7 +54,6 @@ export default class Autofill {
         await this.currentWindow.focus();
 
         const tagName = await webElement.getTagName();
-
         if (tagName === "select") {
           await this.setValueToSelectbox(webElement, inputValueSet.inputValue);
           continue;
@@ -193,14 +212,16 @@ export default class Autofill {
   private async getWebElementsByRegexXpath(
     locator: string
   ): Promise<WebElement[]> {
-    const regex = new RegExp(locator);
+    const regex = new RegExp(locator.toLocaleUpperCase());
     if (!this.xpathList) {
       if (this.pageSource === null) {
         this.pageSource = await this.client.getCurrentPageSource();
       }
       this.xpathList = this.getAllXpath(this.pageSource);
     }
-    const matchXpath = this.xpathList.filter((xpath) => regex.test(xpath));
+    const matchXpath = this.xpathList.filter((xpath) => {
+      return regex.test(xpath.toLocaleUpperCase());
+    });
 
     return Promise.all(
       matchXpath.map(async (xpath) => {
